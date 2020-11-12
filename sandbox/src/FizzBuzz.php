762219714552ba0c62975dceb9c4d6f328e09572
Quentin Darphel <quentin.darphel@etudiant.univ-lr.fr>
2020-11-12 09:09:19 +0100

    FizzBuzz_Test_1_TDD_PHP

--- sandbox/src/FizzBuzz.php
+++ sandbox/src/FizzBuzz.php
@@ -1,7 +1,19 @@
 <?php
 
+namespace App;
+class FizzBuzz{
 
-class FizzBuzz
-{
-
+    /*Test si le nombre est un multiple de 3 et 5, de 3, de 5 ou d'aucun*/
+    public function compte($number){
+        if($number%3==0){
+            if($number%5==0){
+                return "fizzbuzz";
+            }
+            return "fizz";
+        }
+        if($number%5==0){
+            return "buzz";
+        }
+        return $number;
+    }
 }
--- sandbox/test/FizzBuzzTest.php
+++ sandbox/test/FizzBuzzTest.php
@@ -1,9 +1,27 @@
 <?php
 
-
+namespace App;
 use PHPUnit\Framework\TestCase;
 
-class FizzBuzzTest extends TestCase
-{
+class FizzBuzzTest extends TestCase{
 
+    public function test_3_return_fizz(){
+        $partie = new FizzBuzz();
+        $this->assertSame("fizz", $partie->compte(3));
+    }
+
+    public function test_5_return_buzz(){
+        $partie = new FizzBuzz();
+        $this->assertSame("buzz", $partie->compte(5));
+    }
+
+    public function test_15_return_fizzbuzz(){
+        $partie = new FizzBuzz();
+        $this->assertSame("fizzbuzz", $partie->compte(15));
+    }
+
+    public function test_8_return_8(){
+        $partie = new FizzBuzz();
+        $this->assertSame(8, $partie->compte(8));
+    }
 }
