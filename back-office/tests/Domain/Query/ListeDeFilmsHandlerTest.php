cdf8da38bb940f9dae00eb8bff757ec80bad468f
Exilas <alexispiscart@outlook.fr>
2020-11-30 12:02:27 +0100

    Fix : corriger nom méthode de test pour obtenir la liste de films

--- back-office/tests/Domain/Query/ListeDeFilmsHandlerTest.php
+++ back-office/tests/Domain/Query/ListeDeFilmsHandlerTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
 
 class ListeDeFilmsHandlerTest extends TestCase
 {
-    public function test_obtenir_la_liste_de_films_disponibles(){
+    public function test_obtenir_la_liste_de_films_disponibles_interroge_1_catalogue(){
         // Arrange
         $requete=new ListeDeFilmsQuery();
         $catalogueDeFilms = $this->createMock(CatalogueDeFilms::class);
