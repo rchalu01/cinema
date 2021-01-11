e1506c4883b8489e58698ee7ff82bd194d3d04e2
Romain Chalumeau <romain.chalumeau@etudiant.univ-lr.fr>
2021-01-11 11:48:57 +0100

    etape3 api

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
